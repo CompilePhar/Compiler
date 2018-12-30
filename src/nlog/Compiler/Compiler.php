00357dd4edd35a86707dd585549c07ea2fbf0ffa
NLOG <sorisem4106@naver.com>
2018-12-30 10:14:59 +0900

    Update Compiler.php

--- src/nlog/Compiler/Compiler.php
+++ src/nlog/Compiler/Compiler.php
@@ -3,18 +3,13 @@
 namespace nlog\Compiler;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\command\ConsoleCommandSender;
 
-class Compiler extends PluginBase implements Listener {
-
+class Compiler extends PluginBase {
   public function onEnable() {
-	$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	
 	$this->getServer()->dispatchCommand(new ConsoleCommandSender(), "makeserver");
 	$this->getServer()->dispatchCommand(new ConsoleCommandSender(), "stop");
   }
-  
 }
 
 ?>
